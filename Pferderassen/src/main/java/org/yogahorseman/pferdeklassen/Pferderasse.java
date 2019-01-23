53214cb40058ea841adc444ba5ac63cb1dfa4b18
Ulrich Vollmer <ulrichvollmer82@googlemail.com>
2019-01-23 11:34:16 +0100

    First commit
    add additional git files

---
+++ Pferderassen/src/main/java/org/yogahorseman/pferdeklassen/Pferderasse.java
@@ -0,0 +1,25 @@
+package org.yogahorseman.pferdeklassen;
+
+public class Pferderasse {
+	
+	/*
+	 * Git youtube:
+	 * 
+	 * https://www.youtube.com/watch?v=cdsMIX9gB94
+	 * 
+	 * https://www.youtube.com/watch?v=mvnPj_0_GmE
+	 * 
+	 * eclipse push new project to git:
+	 * https://www.youtube.com/watch?v=Huwf0TgWrOw
+	 * 
+	 * 
+	 * Interessant:
+	 * 
+	 * https://www.youtube.com/watch?v=j--FJmbb3P4
+	 * 
+	 * Software Architektur:
+	 * https://www.youtube.com/watch?v=j--FJmbb3P4
+	 * 
+	 */
+
+}
